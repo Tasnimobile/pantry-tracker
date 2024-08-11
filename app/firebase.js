d5064708d817fc069a1cc128754ab71d507c3197
Tasnimobile <tahsinulm2026@gmail.com>
2024-08-10 21:22:07 -0400

    Added Item button

--- app/firebase.js
+++ app/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+//import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore'
 import { collection } from 'firebase/firestore';
 
@@ -22,6 +22,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const firestore = getFirestore(app)
 export {app, firestore}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client"
 
-import { Box, Stack, Typography, Button } from '@mui/material'
+import { Box, Stack, Typography, Button, Modal, TextField } from '@mui/material'
 import { firestore } from './firebase';
-import { collection, getDocs, query} from 'firebase/firestore';
+import { collection, getDocs, query, doc, setDoc, deleteDoc, getDoc} from 'firebase/firestore';
 import { useEffect, useState} from 'react';
 
 // const item = [
@@ -17,23 +17,80 @@ import { useEffect, useState} from 'react';
 //   'cucumber'
 // ]
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  gap: 1,
+  display: 'flex',
+  flexDirection: 'column',
+};
+
 
 export default function Home() {
   const [pantry, setPantry] = useState([])
-  useEffect(() => {
-    const updatePantry = async () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [itemName, setItemName] = useState('')
+
+  const updatePantry = async () => {
       const snapshot = query(collection(firestore, 'pantry'))
       const docs = await getDocs(snapshot)
       const pantryList = []
       docs.forEach((doc) => {
-        pantryList.push(doc.id)
+        pantryList.push({ name: doc.id, ...doc.data() })
       })
       console.log(pantryList)
       setPantry(pantryList)
-    }
+  }
+  
+  useEffect(() => {
     updatePantry()
-    
   }, [])
+
+  const normalizeItemName = (item) => {
+    return item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+  }
+
+
+  const addItem = async (item) => {
+    const fixedItem = normalizeItemName(item)
+    const docRef = doc(collection(firestore, 'pantry'), fixedItem)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { count } = docSnap.data()
+      await setDoc(docRef, { count: count + 1 })
+    }
+    else {
+      await setDoc(docRef, {count: 1})
+    }
+    await updatePantry()
+    
+  }
+
+  const removeItem = async (item) => {
+    const fixedItem = normalizeItemName(item)
+    const docRef = doc(collection(firestore, 'pantry'), fixedItem)
+    const docSnap = await (getDoc(docRef))
+    if (docSnap.exists()) {
+      const { count } = docSnap.data()
+      if (count === 1) {
+        await deleteDoc(docRef)
+      }
+      else {
+        await setDoc(docRef, {count: count - 1})
+      }
+    }
+    await updatePantry()
+  }
+
   return (
     <Box
       width="100vw"
@@ -44,40 +101,81 @@ export default function Home() {
       alignItems={'center'}
       gap = {2}
     >
-      <Button variant='contained'>Add</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width='100%' height='55px' direction={'row'} spacing={2}>
+            <TextField id='outlined-basic'
+              label='Item'
+              variant='outlined'
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value) } />
+            <Button variant="contained"
+              onClick={() => {
+                addItem(itemName)
+                setItemName(' ')
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+          
+        </Box>
+      </Modal>
+      <Button variant='contained'
+      onClick={handleOpen}
+      >Add</Button>
       
       <Box border={'2px solid #333'}>
 
       <Box width="800px" height="70px" bgcolor={'#60f565'}>
         <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
           Pantry Items
-        </Typography>
+          </Typography>
+          
       </Box>
       <Stack width="800px" height="300px" spacing={3} overflow={'auto'}>
-        {pantry.map((i) => (
-          <Box
-            key={i}
-            width="100%"
-            height="100px"
-            display={'flex'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            bgcolor={'#f0f0f0'}
-          >
-            <Typography
-              variant={'h3'}
-              color={'#333'}
-              textAlign={'center'}
-            >
-              
-              {i.charAt(0).toUpperCase() + i.slice(1)}
-            </Typography>  
-          </Box>
-        ))}
+          {pantry.map(({ name, count }) => (
+            <Stack key={name} direction={'row'} spacing={2} justifyContent={'center'} alignContent={'space-between'}>
+            <Box
+              key={name}
+              width="100%"
+              height="100px"
+              display={'flex'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              bgcolor={'#f0f0f0'}
+              >
+                <Stack direction="row" spacing={10} alignItems="center">
+                  <Typography
+                    variant={'h3'} color={'#333'} textAlign={'center'}>
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                  </Typography>  
+                  <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
+                    Quantity: {count}
+                  </Typography>
+                </Stack>
+            </Box>
+              <Button variant='contained' color = 'error' onClick={() => removeItem(name) }>
+                Remove
+            </Button>
+          </Stack>
+          ))}
         
 
-      </Stack>
+        </Stack>
+            
       </Box>
+      
       </Box>
   );
 }
